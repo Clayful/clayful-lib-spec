290ab143b96d926b454a58e5522d663f89c08de5
Daeik <daeik1222@gmail.com>
2016-08-07 13:38:29 +0900

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const _ = require('lodash');
+const gulp = require('gulp');
+const jeditor = require('gulp-json-editor');
+const rename = require('gulp-rename');
+
+gulp.task('extract', () => {
+
+	const listToMap = api => api.list.reduce((map, a) => {
+
+		const [className, methodName] = a.module.split('.');
+		const options = [a.method, a.path];
+		const aliases = api.aliases.reduce((all, replacer) => {
+
+			const [from, to] = replacer;
+
+			if (methodName.indexOf(from) >= 0) {
+
+				all.push(methodName.replace(from, to));
+			}
+
+			return all;
+
+		}, []);
+
+		if (aliases.length > 0) options.push(aliases);
+
+		return _.set(map, a.module, options);
+
+	}, {});
+
+	gulp.src('./data/api.json')
+		.pipe(jeditor(listToMap))
+		.pipe(rename(path => path.basename = 'map'))
+		.pipe(gulp.dest('./data'));
+
+});
